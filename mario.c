6fa36a74455c5b4332234934028d2ed0ad4d8a80
varfgehu <varfgehu@users.noreply.github.com>
2020-04-12 12:37:25 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -13,25 +13,25 @@ int main(void)
     {
         height = get_int("Height: ");
 
-        if(height >=1 && height <=8)
+        if (height >= 1 && height <= 8)
         {
-            for(int i = 1; i <=height; i++)
+            for (int i = 1; i <= height; i++)
             {
-                print_space(height-i);
+                print_space(height - i);
                 print_hashmark(i);
                 print_new_line();
             }
         }
     }
-    while(height <1 || height >8);   
+    while (height < 1 || height > 8);   
 }
 
 void print_hashmark(int repeat)
 {
-        for(int i = 0; i <repeat; i++)
-        {
-            printf("#");
-        }
+    for(int i = 0; i <repeat; i++)
+    {
+        printf("#");
+    }
 }
 
 void print_new_line(void)
@@ -41,9 +41,9 @@ void print_new_line(void)
 
 void print_space(int repeat)
 {
-        for(int i = 0; i <repeat; i++)
-        {
-            printf(" ");
-        }
+    for (int i = 0; i < repeat; i++)
+    {
+        printf(" ");
+    }
 }
 
