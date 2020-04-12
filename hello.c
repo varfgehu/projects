d11ad7531c1a3d65b59b5d8f8c4e500225ba4334
varfgehu <varfgehu@users.noreply.github.com>
2020-04-12 11:52:48 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
+// greet user
+
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
+    // ask the user's name
     string name = get_string("What is your name?\n");
+    
+    //print the user's name to terminal
     printf("hello, %s\n", name);
 }
