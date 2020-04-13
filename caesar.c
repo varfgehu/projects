c011bc49ba6fc44c8d4d9a0bdda4f12caf4106df
varfgehu <varfgehu@users.noreply.github.com>
2020-04-13 15:06:59 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -6,7 +6,7 @@
 
 void terminate_program(void);
 
-int main(int argc, string argv[] )
+int main(int argc, string argv[])
 {
     if (argc != 2)
     {
@@ -15,10 +15,10 @@ int main(int argc, string argv[] )
     }
     else
     {
-        for(int i = 0, length = strlen(argv[1]); i < length; i++)
+        for (int i = 0, length = strlen(argv[1]); i < length; i++)
         {
             //printf("%c\n", argv[1][i]);
-            if(argv[1][i] < '0' || argv[1][i] > '9')
+            if (argv[1][i] < '0' || argv[1][i] > '9')
             {
                 printf("Usage: ./caesar key\n");
                 return 1;
@@ -32,28 +32,28 @@ int main(int argc, string argv[] )
 
         printf("ciphertext: ");
 
-        for(int i = 0, length = strlen(plaintext); i < length ; i++)
+        for (int i = 0, length = strlen(plaintext); i < length ; i++)
         {
             if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
             {
-                if( plaintext[i] + key > (int)'z' )
+                if (plaintext[i] + key > (int)'z')
                 {
-                    printf("%c", key - ( (int)'z' - plaintext[i] ) - 1 + (int)'a');
+                    printf("%c", key - ((int)'z' - plaintext[i]) - 1 + (int)'a');
                 }
                 else
                 {
-                    printf("%c", ( (int)plaintext[i] + key) );
+                    printf("%c", ((int)plaintext[i] + key));
                 }
             }
             else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
             {
-                if( plaintext[i] + key > (int)'Z' )
+                if (plaintext[i] + key > (int)'Z')
                 {
-                    printf("%c", key - ( (int)'Z' - plaintext[i] ) - 1 + (int)'A');
+                    printf("%c", key - ((int)'Z' - plaintext[i]) - 1 + (int)'A');
                 }
                 else
                 {
-                    printf("%c", ( (int)plaintext[i] + key) );
+                    printf("%c", ((int)plaintext[i] + key));
                 }
             }
             else
