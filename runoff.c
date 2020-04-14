fc10f1fb445d913f333797fd56cbaa9346f49823
varfgehu <varfgehu@users.noreply.github.com>
2020-04-14 19:33:45 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -201,7 +201,7 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for (int i = 0; i < voter_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if(min != candidates[i].votes && false == candidates[i].eliminated)
         {
