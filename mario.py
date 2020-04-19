a57143ae6049e70fa33129193ab8e3185774c2c4
varfgehu <varfgehu@users.noreply.github.com>
2020-04-19 15:11:11 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -5,7 +5,7 @@ heigth = get_int("Heigth: ")
 while(heigth < 1 or heigth > 8):
     heigth = get_int("Heigth: ")
 
-for i in range(heigth):
-    print(" " * (heigth-i+1), end="")
-    print("#" * (i+1), end="")
+for i in range(1, heigth+1):
+    print(" " * (heigth-i), end="")
+    print("#" * (i), end="")
     print()
