9ebc913d597fa460754c7b5cd88b32a48143983c
varfgehu <varfgehu@users.noreply.github.com>
2020-04-21 17:45:14 +0000

    automated commit by submit50

--- 7.sql
+++ 7.sql
@@ -1,1 +1,5 @@
-SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year = 2010 ORDER BY ratings.rating ASC, movies.title DESC;
+SELECT title, rating
+FROM movies
+	JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.year = 2010
+ORDER BY ratings.rating DESC, movies.title ASC;
