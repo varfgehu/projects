51d0c139fd8850257d1acea07970abf84c340886
varfgehu <varfgehu@users.noreply.github.com>
2020-04-22 12:42:17 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -16,10 +16,8 @@ with open(argv[1], "r") as characters:
     for row in reader:
         name = row['name'].split(" ")
         if len(name) == 2:
-            print("name length 2\n" + name[0] + name[1] + row['house'] + row['birth'] + "\n\n")
-            db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
-            name[0], "None", name[1], row['house'], row['birth'])
+            db.execute("INSERT INTO students (first, last, house, birth) VALUES(?, ?, ?, ?)",
+            name[0], name[1], row['house'], row['birth'])
         else:
-            print("name length 3\n" + name[0] + name[1] + name[2]+ row['house'] + row['birth'] + "\n\n")
             db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
             name[0], name[1], name[2], row['house'], row['birth'])
