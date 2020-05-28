ed03569ceaaf33847b86b9d2bb4f245ddff28b7b
varfgehu <varfalvigergo@gmail.com>
2020-05-28 17:34:25 +0100

    user login and logout implementation.

--- application.py
+++ application.py
@@ -1,13 +1,80 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, jsonify, render_template, request, redirect
+from flask_session import Session
 from flask_socketio import SocketIO, emit
+
+from helpers import login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Store existing channels
+existingChannels = []
+
+# Store logged in users
+usersLoggedIn = []
 
 @app.route("/")
+@login_required
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html", channels=existingChannels)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    # Forget any users
+    session.clear()
+
+    username = request.form.get("username")
+
+    if request.method == "POST":
+        print("login POST detected")
+        if len(username) < 1 or username is '':
+            return render_template("apology.html", message="username can not be empty")
+
+        if username in usersLoggedIn:
+            return render_template("apology.html", message="username has already taken!")
+
+        usersLoggedIn.append(username)
+
+        session['username'] = username
+        print("Username: " + username)
+
+        # Remember the user session on a cookie if the browser is closed
+        session.permanent = True
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """
+    Logout:
+    Logged in users should be able to log out of the site.
+    """
+
+    # Remove username from the taken list
+    usersLoggedIn.remove(session['username'])
+
+    # Forget any user_id
+    session.clear()
+
+
+    # Redirect user to login form
+    return redirect("/")
